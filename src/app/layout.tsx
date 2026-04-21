57e3eb5bf3948f6cfdab4d449d01eb4f30315a9c
M. Rezky Revansyah Suprihono <rezky.revansyah@gmail.com>
2026-04-21 16:14:09 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,6 @@ const amiri = Amiri({
 export const metadata: Metadata = {
   title: "Masjid Jami' Al-Arqom",
   description: "Website Resmi Masjid Jami' Al-Arqom",
-  icons: {
-    icon: "/logo.png",
-    apple: "/logo.png",
-  },
 };
 
 export default function RootLayout({
